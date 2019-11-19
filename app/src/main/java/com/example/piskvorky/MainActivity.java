4a99aeff9f782ae6e22b0632b5049f97a3bc6ee4
vav0188 <45627257+vav0188@users.noreply.github.com>
2019-11-19 16:37:00 +0100

    Update Main and Settings activity

--- app/src/main/java/com/example/piskvorky/MainActivity.java
+++ app/src/main/java/com/example/piskvorky/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.piskvorky;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button novaHra = (Button)findViewById(R.id.btnNewGame);
+        Button nastaveni = (Button)findViewById(R.id.btnSettings);
+        Button zebricek = (Button)findViewById(R.id.btnHighscore);
+
+        novaHra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        nastaveni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        zebricek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HighscoreActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/piskvorky/SettingsActivity.java
+++ app/src/main/java/com/example/piskvorky/SettingsActivity.java
@@ -2,7 +2,14 @@ package com.example.piskvorky;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +17,83 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Switch cross = (Switch) findViewById(R.id.swKrizky);
+        Switch sound = (Switch) findViewById(R.id.swZvuk);
+        Switch music = (Switch) findViewById(R.id.swHudba);
+        Button menu = (Button) findViewById(R.id.btnMenu);
+
+        SharedPreferences prefs = getSharedPreferences("Settings", 0);
+        boolean boolCross = prefs.getBoolean("cross", false);
+        boolean boolSound = prefs.getBoolean("sound", false);
+        boolean boolMusic = prefs.getBoolean("music", true);
+
+        Log.d("checked","cross "+boolCross);
+
+        cross.setChecked(boolCross);
+        sound.setChecked(boolSound);
+        music.setChecked(boolMusic);
+
+
+        cross.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("Settings", 0); // 0 - for private mode
+                SharedPreferences.Editor editor = pref.edit();
+                if (isChecked)
+                {
+                    editor.putBoolean("cross",true);
+                    editor.commit();
+                }
+                else {
+                    editor.putBoolean("cross",false);
+                    editor.commit();
+                }
+            }
+        });
+
+        sound.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("Settings", 0); // 0 - for private mode
+                SharedPreferences.Editor editor = pref.edit();
+                if (isChecked) {
+
+                    editor.putBoolean("sound",true);
+                    editor.commit();
+                } else {
+                    editor.putBoolean("sound",false);
+                    editor.commit();
+                }
+            }
+        });
+
+        music.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("Settings", 0); // 0 - for private mode
+                SharedPreferences.Editor editor = pref.edit();
+                if (isChecked) {
+                    Log.d("checked","ch");
+                    editor.putBoolean("music",true);
+                    editor.commit();
+                } else {
+                    editor.putBoolean("music",false);
+                    editor.commit();
+                }
+            }
+        });
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
     }
 }
